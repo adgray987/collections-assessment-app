dce73d19f3df387a4a3e92da129f80e2f3eaf576
Adam Gray <36088026+adgray987@users.noreply.github.com>
2021-08-29 19:32:15 -0400

    Add explanatory note about assessments

--- assessment_app_beta.py
+++ assessment_app_beta.py
@@ -4,15 +4,14 @@ import pandas as pd
 import altair as alt
 
 st.title('Collections Assessment App')
-st.write('This is a prototype of a Streamlit app for exploring collections assessment data exported from ArchivesSpace. You can adjust filters for ratings and survey size using the filters on the left, then view summary statistics for ratings and special formats based on filtered results. You can also view the filtered results themselves.')
+st.markdown('Libraries, archives, and museums collect data about the stuff on their shelves (and drives) using a type of survey called a *collections assessment*. They use this data to understand their backlogs of "unprocessed" material and make data-informed decisions about which materials they should prioritize for preservation and processing, and how to reduce their backlogs to better serve their users.')
+st.markdown('This is a prototype of a Streamlit app for exploring collections assessment data exported from ArchivesSpace, an open-source collections management system for archives. You can adjust filters for ratings and survey size using the filters on the left, then view summary statistics for ratings and special formats based on filtered results. You can also view the filtered results themselves.')
 
-
-# Load CSV with st.file_uploader()
-
+# Get csv
 sample_file = "https://raw.githubusercontent.com/adgray987/collections-assessment-eda/main/data_raw/assessment_list_report.csv"
 data = pd.read_csv(sample_file, header=3)
 
-
+# Load CSV with st.file_uploader()
 # Upload file option
 ##uploaded_file = st.file_uploader(label="Upload an Assessment Record List Report (CSV) from ArchivesSpace.",
 ##                                 help="Upload an Assessment Record List Report (CSV) from ArchivesSpace. The column headers have to be in row 3 or the data may not load properly. Large files may take longer to process.")
@@ -73,7 +72,7 @@ for col in df.columns:
 
 df.dropna(how="any", inplace=True, subset=ratings_names)
 df[ratings_names] = df[ratings_names].astype("int64")
-# Parse numbers from strings in extent field if units are known and consitent. Otherwise comment out line below. 
+# Parse numbers from strings in extent field if units are known and consitent. Otherwise comment out line below.
 ##df["Surveyed Extent (cubic feet)"] = df["Surveyed Extent"].str.extract(r"([\d\.]+)")[0].astype("float").round().replace(0, 1).astype("int")
 # Say units are cubic feet for example
 df["Surveyed Extent (cubic feet)"] = df["Surveyed Extent"].astype("float").round().replace(0, 1).astype("int")
@@ -150,7 +149,7 @@ with st.expander("Summary stats"):
         ).properties(title="Distribution of Survey Size")
 
     summary = subset["Surveyed Extent (cubic feet)"].rename("Surveyed Extent").describe()
-    
+
     statcol1, statcol2 = st.columns([2, 1])
     with statcol1:
         st.altair_chart(extents_hist, use_container_width=True)
@@ -160,7 +159,7 @@ with st.expander("Summary stats"):
 
     # Display extents chart
     st.altair_chart(extents_bar, use_container_width=True)
-    
+
     st.subheader("Ratings counts")
 
     col1, col2 = st.columns(2)
@@ -183,7 +182,7 @@ with st.expander("Summary stats"):
         ), use_container_width=True)
 
     st.subheader("Documentation, Special Formats, etc.")
-    
+
     # Counting bool-type cols
     bool_counts = subset[bool_cols].apply(lambda x: x.value_counts())
     bool_display = bool_counts.T["Yes"].dropna().rename("# of Collections")
@@ -199,7 +198,7 @@ with st.expander("Summary stats"):
     )
     st.altair_chart(bool_chart, use_container_width=True)
 
-    
+
 # Results table
 with st.expander("Filtered records"):
     # Can't nest expanders
@@ -210,7 +209,5 @@ with st.expander("Filtered records"):
     st.write("Sort results by clicking on a column header.")
     st.write(subset[column_picker])
 
-st.markdown("*Due to the customizable nature of the assessment module and its use as an internal collections management tool, I've used fake data and made some assumptions about how people collect and use it (e.g., surveyed extent units are in cubic feet).*")
+st.markdown("*Due to the customizable nature of the ArchivesSpace assessment module and its use as an internal collections management tool, I've used fake data and made some assumptions about how people collect and use it (e.g., surveyed extent units are in cubic feet).*")
 st.markdown("*Get in touch with any feedback at a d g r a y 9 8 7 at gmail dot com. I made this as a labor of love, and I would love to hear from you. If you want to check out or reuse (and improve!) the code, it's [here](https://github.com/adgray987/collections-assessment-app).*")
-
-
